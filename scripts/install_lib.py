f4c45e5ca27b6a19feedb41b8f740d90218f9689
sysheng <sysheng21@cse.cuhk.edu.hk>
2023-04-18 15:00:42 +0800

    install cachelib with a specific commit id

--- scripts/install_lib.py
+++ scripts/install_lib.py
@@ -4,10 +4,24 @@ import os
 import sys
 import subprocess
 
-from common import *
-
 filename = sys.argv[0]
 is_clear = False # whether to clear intermediate files (e.g., tarball files)
+
+# (1) Install python libraries (some required by scripts/common.py)
+
+pylib_requirement_filepath = "scripts/requirements.txt"
+#prompt(filename, "install python libraries based on {}...".format(pylib_requirement_filepath))
+print("{}: {}".format(filename, "install python libraries based on {}...".format(pylib_requirement_filepath)))
+pylib_install_cmd = "pip3 install -r {}".format(pylib_requirement_filepath)
+
+pylib_install_subprocess = subprocess.run(pylib_install_cmd, shell=True)
+if pylib_install_subprocess.returncode != 0:
+    #die(filename, "failed to install python libraries based on {}".format(pylib_requirement_filepath))
+    print("{}: {}".format(filename, "failed to install python libraries based on {}".format(pylib_requirement_filepath)))
+    sys.exit(1)
+
+# Include common module for the following installation
+from common import *
 
 lib_dirpath = "lib"
 if not os.path.exists(lib_dirpath):
@@ -16,11 +30,11 @@ if not os.path.exists(lib_dirpath):
 else:
     dump(filename, "{} exists".format(lib_dirpath))
 
-# (1) Install boost 1.81.0
+# (2) Install boost 1.81.0
 
 boost_download_filepath = "lib/boost_1_81_0.tar.gz"
 if not os.path.exists(boost_download_filepath):
-    prompt(filename, "Download {}...".format(boost_download_filepath))
+    prompt(filename, "download {}...".format(boost_download_filepath))
     boost_download_cmd = "cd lib && wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz"
 
     boost_download_subprocess = subprocess.run(boost_download_cmd, shell=True)
@@ -31,7 +45,7 @@ else:
 
 boost_decompress_dirpath = "lib/boost_1_81_0"
 if not os.path.exists(boost_decompress_dirpath):
-    prompt(filename, "Decompress {}...".format(boost_download_filepath))
+    prompt(filename, "decompress {}...".format(boost_download_filepath))
     boost_decompress_cmd = "cd lib && tar -xzvf boost_1_81_0.tar.gz"
 
     boost_decompress_subprocess = subprocess.run(boost_decompress_cmd, shell=True)
@@ -42,7 +56,7 @@ else:
 
 boost_install_dirpath = "lib/boost_1_81_0/install"
 if not os.path.exists(boost_install_dirpath):
-    prompt(filename, "Install lib/boost from source...")
+    prompt(filename, "install lib/boost from source...")
     boost_install_cmd = "cd {} && ./bootstrap.sh --with-libraries=log,thread,system,filesystem,program_options,test,json --prefix=./install && sudo ./b2 install".format(boost_decompress_dirpath)
     # Use the following command if your set prefix as /usr/local which needs to update Linux dynamic linker
     #boost_install_cmd = "cd {} && ./bootstrap.sh --with-libraries=log,thread,system,filesystem,program_options,test --prefix=./install && sudo ./b2 install && sudo ldconfig".format(boost_decompress_dirpath)
@@ -61,12 +75,33 @@ if is_clear:
     if boost_clear_subprocess.returncode != 0:
         die(filename, "failed to clear".format(boost_download_filepath))
 
-# (2) Install python libraries
+# (3) Install cachelib (commit ID: 3d475f6)
 
-pylib_requirement_filepath = "scripts/requirements.txt"
-prompt(filename, "Install python libraries based on {}...".format(pylib_requirement_filepath))
-pylib_install_cmd = "pip3 install -r {}".format(pylib_requirement_filepath)
+cachelib_clone_dirpath = "lib/cachelib"
+if not os.path.exists(cachelib_clone_dirpath):
+    prompt(filename, "clone cachelib into {}...".format(cachelib_clone_dirpath))
+    cachelib_clone_cmd = "cd lib && git clone https://github.com/facebook/CacheLib.git"
 
-pylib_install_subprocess = subprocess.run(pylib_install_cmd, shell=True)
-if pylib_install_subprocess.returncode != 0:
-    die(filename, "failed to install python libraries based on {}".format(pylib_requirement_filepath))
+    cachelib_clone_subprocess = subprocess.run(cachelib_clone_cmd, shell=True)
+    if cachelib_clone_subprocess.returncode != 0:
+        die(filename, "failed to clone cachelib into {}".format(cachelib_clone_dirpath))
+else:
+    dump(filename, "{} exists".format(cachelib_clone_dirpath))
+
+cachelib_targetcommit = "3d475f6"
+cachelib_checkversion_cmd = "cd {} && git log --format=\"%H\" -n 1".format(cachelib_clone_dirpath)
+cachelib_checkversion_subprocess = subprocess.run(cachelib_checkversion_cmd, shell=True, capture_output=True)
+if cachelib_checkversion_subprocess.returncode != 0:
+    die(filename, "failed to get the latest commit ID of cachelib")
+else:
+    cachelib_checkversion_outputbytes = cachelib_checkversion_subprocess.stdout
+    cachelib_checkversion_outputstr = cachelib_checkversion_outputbytes.decode("utf-8")
+    if cachelib_targetcommit not in cachelib_checkversion_outputstr:
+        prompt(filename, "the latest commit ID of cachelib is {} -> reset cache lib to commit {}...".format(cachelib_checkversion_outputstr, cachelib_targetcommit))
+        cachelib_reset_cmd = "cd {} && git reset --hard {}".format(cachelib_clone_dirpath, cachelib_targetcommit)
+        
+        cachelib_reset_subprocess = subprocess.run(cachelib_reset_cmd, shell=True)
+        if cachelib_reset_subprocess.returncode != 0:
+            die(filename, "failed to reset cachelib")
+    else:
+        dump(filename, "the latest commit ID of cachelib is already {}".format(cachelib_targetcommit))
