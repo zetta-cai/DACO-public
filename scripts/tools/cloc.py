adeb304bddc2179120b70b80200386f941d96d20
sysheng <sysheng21@cse.cuhk.edu.hk>
2024-03-26 01:41:29 +0800

    update for snapshot preliminary results

--- scripts/tools/cloc.py
+++ scripts/tools/cloc.py
@@ -30,50 +30,52 @@ exclude_files += ")\""
 # --fullpath add the current working directory (pwd) ahead of --not-match-d and --not-match-f
 exclude_command = "--exclude-ext={} --fullpath --not-match-d={} --not-match-f={}".format(exclude_exts, exclude_dirs, exclude_files)
 
+# NOTE: all interfacing code has been excluded during the following code counting
+
+def countLOC(c_dirs, hintstr):
+    c_dir_str = ""
+    for tmp_dir in c_dirs:
+        c_dir_str += tmp_dir + " "
+
+    LogUtil.prompt(Common.scriptname, "Count LOC for {}...".format(hintstr))
+    tmp_cloc_cmd = "cloc {} {}".format(exclude_command, c_dir_str)
+
+    tmp_cloc_subprocess = SubprocessUtil.runCmd(tmp_cloc_cmd, keep_silent = True)
+    if tmp_cloc_subprocess.returncode != 0:
+        LogUtil.die(Common.scriptname, "failed to count LOC for {} (errmsg: {})".format(hintstr, SubprocessUtil.getSubprocessErrstr(tmp_cloc_subprocess)))
+    else:
+        tmp_outputstr = SubprocessUtil.getSubprocessOutputstr(tmp_cloc_subprocess)
+        print(tmp_outputstr)
+    print("")
+
 # (1) Count LOC for all C/C++ source code
 
 c_dirs = ["src"]
-for tmp_dir in c_dirs:
-    LogUtil.prompt(Common.scriptname, "Count LOC for all C/C++ source code in {}...".format(tmp_dir))
-    tmp_cloc_cmd = "cloc {} {}".format(exclude_command, tmp_dir)
+countLOC(c_dirs, "all")
 
-    tmp_cloc_subprocess = SubprocessUtil.runCmd(tmp_cloc_cmd, keep_silent = True)
-    if tmp_cloc_subprocess.returncode != 0:
-        LogUtil.die(Common.scriptname, "failed to count LOC for all C/C++ source code in {} (errmsg: {})".format(tmp_dir, SubprocessUtil.getSubprocessErrstr(tmp_cloc_subprocess)))
-    else:
-        tmp_outputstr = SubprocessUtil.getSubprocessOutputstr(tmp_cloc_subprocess)
-        print(tmp_outputstr)
-print("")
+# (2) Count LOC for client C/C++ source code
 
-# (2) Count LOC for design-related C/C++ source code
+client_c_dirs = ["src/benchmark/client*", "src/client.c"]
+countLOC(client_c_dirs, "client")
 
-# "src/message/*/*/covered*"
-design_c_dirs = ["src/cache/covered*", "src/core", "src/edge/*/covered*"]
-design_c_dir_str = ""
-for tmp_dir in design_c_dirs:
-    design_c_dir_str += tmp_dir + " "
+# (3) Count LOC for cloud C/C++ source code
 
-LogUtil.prompt(Common.scriptname, "Count LOC for design-related C/C++ source code in {}...".format(design_c_dir_str))
-tmp_cloc_cmd = "cloc {} {}".format(exclude_command, design_c_dir_str)
+cloud_c_dirs = ["src/cloud", "src/cloud.c"]
+countLOC(cloud_c_dirs, "cloud")
 
-tmp_cloc_subprocess = SubprocessUtil.runCmd(tmp_cloc_cmd, keep_silent = True)
-if tmp_cloc_subprocess.returncode != 0:
-    LogUtil.die(Common.scriptname, "failed to count LOC for design-related C/C++ source code in {} (errmsg: {})".format(tmp_dir, SubprocessUtil.getSubprocessErrstr(tmp_cloc_subprocess)))
-else:
-    tmp_outputstr = SubprocessUtil.getSubprocessOutputstr(tmp_cloc_subprocess)
-    print(tmp_outputstr)
-print("")
+# (4) Count LOC for cache C/C++ source code
 
-# (3) Count LOC for python scripts
+# "src/cli", "src/event", "src/statistics", "src/cliutil.c", "src/evaluator.c", "src/simulator.c", "src/total_statistics_loader.c", "src/trace_preprocessor.c"
+cache_c_dirs = ["src/cache", "src/common", "src/concurrency", "src/cooperation", "src/core", "src/edge", "src/hash", "src/message", "src/nework", "src/workload", "src/dataset_loader.c", "src/edge.c"]
+countLOC(cache_c_dirs, "cache")
+
+# (5) Count LOC for COVERED C/C++ source code
+
+# "src/message/*/*/covered*", "src/cache/covered_local_cache.*", "src/cache/covered_cache_custom_func_param.*", "src/edge/covered*"
+covered_c_dirs = ["src/cache/covered", "src/core", "src/edge/beacon_server/covered*", "src/edge/cache_server/covered*"]
+countLOC(covered_c_dirs, "covered")
+
+# (6) Count LOC for python scripts
 
 py_dirs = ["scripts"]
-for tmp_dir in py_dirs:
-    LogUtil.prompt(Common.scriptname, "Count LOC for python script code in {}...".format(tmp_dir))
-    tmp_cloc_cmd = "cloc {} {}".format(exclude_command, tmp_dir)
-
-    tmp_cloc_subprocess = SubprocessUtil.runCmd(tmp_cloc_cmd, keep_silent = True)
-    if tmp_cloc_subprocess.returncode != 0:
-        LogUtil.die(Common.scriptname, "failed to count python script code in {} (errmsg: {})".format(tmp_dir, SubprocessUtil.getSubprocessErrstr(tmp_cloc_subprocess)))
-    else:
-        tmp_outputstr = SubprocessUtil.getSubprocessOutputstr(tmp_cloc_subprocess)
-        print(tmp_outputstr)
+countLOC(py_dirs, "scripts")
