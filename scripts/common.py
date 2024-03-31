dba6aedb63834555a3a32c726e4c27b2feae71ce
sysheng <sysheng21@cse.cuhk.edu.hk>
2024-03-31 22:11:19 +0800

    update

--- scripts/common.py
+++ scripts/common.py
@@ -98,11 +98,16 @@ class Common:
 
     ## (5) Experiment settings
 
-    exp_round_number = 1 # Run each experiment for exp_round_number times
+    exp_round_number = 2 # Run each experiment for exp_round_number times
 
     ## (6) Real-network settings
 
     # Initialized single-trip time (RTT / 2) (TODO: update based on your real-network testbed)
+    # NOTE: used for the first round (morning, Mar. 31, 2024)
     alicloud_avg_clientedge_latency_us = 750
     alicloud_avg_crossedge_latency_us = 10650
     alicloud_avg_edgecloud_latency_us = 30000
+    # NOTE: used for the second round (afternoon, Mar. 31, 2024)
+    alicloud_avg_clientedge_latency_us = 742
+    alicloud_avg_crossedge_latency_us = 12140
+    alicloud_avg_edgecloud_latency_us = 29985
