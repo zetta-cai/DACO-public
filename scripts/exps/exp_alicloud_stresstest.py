7fd5c315ad42b301d0ca5b2e98cb4eb938cedb20
sysheng <sysheng21@cse.cuhk.edu.hk>
2024-09-11 18:16:24 +0800

    fix script issues

--- scripts/exps/exp_alicloud_stresstest.py
+++ scripts/exps/exp_alicloud_stresstest.py
@@ -30,8 +30,8 @@ exp_default_settings = {
     "realnet_expname": "",
 }
 # NOTE: run lrb, glcache, and segcache at last due to slow warmup issue of lrb (may be caused by model retraining), and memory usage issue of segcache and glcache (may be caused by bugs on segment-level memory management) -> TODO: if no results of the above baselines due to program crashes, please provide more DRAM memory (or swap memory), and run them again with sufficient time (may be in units of hours or days) for warmup and cache stable performance
-#cache_names = ["covered", "shark", "bestguess", "arc+", "cachelib+", "fifo+", "frozenhot+", "gdsf+", "lfu+", "lhd+", "s3fifo+", "sieve+", "wtinylfu+", "lrb+", "glcache+", "segcache+"]
-cache_names = ["covered", "gdsf+", "lhd+"] # TMPDEBUG
+# cache_names = ["covered", "shark", "bestguess", "shark+arc", "shark+cachelib", "shark+fifo", "shark+frozenhot", "shark+gdsf", "shark+lfu", "shark+lhd", "shark+s3fifo", "shark+sieve", "shark+wtinylfu", "shark+lrb", "shark+glcache", "shark+segcache"]
+cache_names = ["covered", "shark+gdsf", "shark+lhd"] # TMPDEBUG
 
 current_origin_name = "shanghai" # shanghai, singapore, silicon
 
--- scripts/exps/exp_parameter_latency.py
+++ scripts/exps/exp_parameter_latency.py
@@ -33,7 +33,7 @@ exp_default_settings = {
     "propagation_latency_edgecloud_rbound_us": 66000
 }
 # NOTE: run lrb, glcache, and segcache at last due to slow warmup issue of lrb (may be caused by model retraining), and memory usage issue of segcache and glcache (may be caused by bugs on segment-level memory management) -> TODO: if no results of the above baselines due to program crashes, please provide more DRAM memory (or swap memory), and run them again with sufficient time (may be in units of hours or days) for warmup and cache stable performance
-# cache_names = ["covered", "shark", "bestguess", "arc+", "cachelib+", "fifo+", "frozenhot+", "gdsf+", "lfu+", "lhd+", "s3fifo+", "sieve+", "wtinylfu+", "lrb+", "glcache+", "segcache+"]
+# cache_names = ["covered", "shark", "bestguess", "shark+arc", "shark+cachelib", "shark+fifo", "shark+frozenhot", "shark+gdsf", "shark+lfu", "shark+lhd", "shark+s3fifo", "shark+sieve", "shark+wtinylfu", "shark+lrb", "shark+glcache", "shark+segcache"]
 cache_names = ["covered", "shark+gdsf", "shark+lhd"] # NOTE: just for fast evaluation -> you can use the above cache list if with time
 # NOTE: NO need to run default WAN delay settings, which is the same as previous experiments (performance against existing and extended methods)
 large_wan_name = "large"
